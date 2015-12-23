1c28dd900659e003dce2fe58f861ca6367b19ff4
Stephen Hodge <Stephen.Hodge@spongeuk.com>
2015-12-23 17:42:23 +0000

    f

--- RadioPi.py
+++ RadioPi.py
@@ -52,41 +52,41 @@ def ledMode(Mode):
 # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
 
-        print adcnum
-        print "---"
+    print adcnum
+    print "----"
 
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    GPIO.output(cspin, True)
 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
+    GPIO.output(clockpin, False)  # start clock low
+    GPIO.output(cspin, False)     # bring CS low
 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
+    commandout = adcnum
+    commandout |= 0x18  # start bit + single-ended bit
+    commandout <<= 3    # we only need to send 5 bits here
+    for i in range(5):
+        if (commandout & 0x80):
+                GPIO.output(mosipin, True)
+        else:
+                GPIO.output(mosipin, False)
+        commandout <<= 1
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
+    adcout = 0
+    # read in one empty bit, one null bit and 10 ADC bits
+    for i in range(12):
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
+        adcout <<= 1
+        if (GPIO.input(misopin)):
+            adcout |= 0x1
 
-        GPIO.output(cspin, True)
+    GPIO.output(cspin, True)
 
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
+    adcout >>= 1       # first bit is 'null' so drop it
+    return adcout
 
 def playStation(trim_pot_tune):
 
@@ -165,7 +165,7 @@ def playStation(trim_pot_tune):
                 print currentChannel
                 processStream = subprocess.Popen("./RadioStreams/PremareLondon.sh", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
 
-        lastTrimPot_tune = trim_pot_tune
+    lastTrimPot_tune = trim_pot_tune
 
 def adjustVolume(trim_pot_vol):
 
@@ -183,7 +183,8 @@ def adjustVolume(trim_pot_vol):
         os.system(set_vol_cmd)  # set volume
         print 'volume set to:'
         print set_vol_cmd
-        lastTrimPot_vol = trim_pot_vol
+
+    lastTrimPot_vol = trim_pot_vol
 
 
 while True:
