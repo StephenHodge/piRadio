183814c607bdff1167a7fe37dc07a77c97d6da38
Stephen Hodge <Stephen.Hodge@spongeuk.com>
2015-12-23 11:57:17 +0000

    potentiometer_vol

--- RadioPi.py
+++ RadioPi.py
@@ -162,12 +162,14 @@ def playStation(trim_pot_tune):
         lastTrimPot_tune = trim_pot_tune
 
 def adjustVolume(trim_pot_vol):
-    
+
     #trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
     #print trim_pot
+
     trimDifference_vol = abs(lastTrimPot_vol - trim_pot_vol)
 
     if trimDifference_vol >= trimTollerance:
+
         set_volume = trim_pot_vol / 10.24       # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
         set_volume = round(set_volume)          # round out decimal value
         set_volume = int(set_volume)            # cast volume as integer
